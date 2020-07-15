0f6ae2b303159ac2c6439a005e4e48f223cc4118
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-15 02:42:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  
+  numbers = [1, 2, 3]
+  new_numbers = []
+  numbers.each do |number|
+    result = number ** 2 
+    new_numbers << result 
+  end
+  return new_numbers
 end
