3f075dcf0523d38adcd251294a70a20733272756
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-15 02:36:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,11 @@
 def square_array(array)
-  array = ["1", "2", "3"]
-  array.each do |array|
-    
+  old_array = ["1", "2", "3"]
+  counter = 0 
+  new_array = []
+  while counter <= old_array.length 
+    result[counter] = old_array[counter] ** 2
+    counter += 1
+  new_array << result[counter]
   end
+  return new_array 
 end
